111a4eb36477dfbeb53c7d36ac6af83be4a1e862
VictoriaKirplyuk <meosh99@bk.ru>
2022-05-17 23:27:30 +0300

    Added test elements

--- src/components/Pages/TestPage/TestPage.tsx
+++ src/components/Pages/TestPage/TestPage.tsx
@@ -1,9 +1,14 @@
 import s from './TestPage.module.css'
+import SuperInputText from "../../../superComponents/SuperInputText/SuperInputText";
+import SuperButton from "../../../superComponents/SuperButton/SuperButton";
+import SuperCheckbox from "../../../superComponents/SuperCheckbox/SuperCheckbox";
 
 export const TestPage = () => {
     return (
         <div className={s.container}>
-            TEST PAGE
+            <SuperInputText/>
+            <SuperButton>click</SuperButton>
+            <SuperCheckbox/>
         </div>
     )
 }
--- src/superComponents/SuperCheckbox/SuperCheckbox.tsx
+++ src/superComponents/SuperCheckbox/SuperCheckbox.tsx
@@ -11,12 +11,12 @@ type SuperCheckboxPropsType = DefaultInputPropsType & {
 
 const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        type,
         onChange, onChangeChecked,
         className, spanClassName,
-        children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
+        children,
 
-        ...restProps// все остальные пропсы попадут в объект restProps
+        ...restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
@@ -34,10 +34,10 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
                 onChange={onChangeCallback}
                 className={finalInputClassName}
 
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
+                {...restProps}
             />
             {children && <span className={s.spanClassName}>{children}</span>}
-        </label> // благодаря label нажатие на спан передастся в инпут
+        </label>
     )
 }
 
